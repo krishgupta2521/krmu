3f97ccfbc96862a1c8d4c46dc69f28ae7aa8d17d
Umang Saini <sainiumang505@gmail.com>
2025-02-20 09:27:18 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["thumbs.dreamstime.com", "media.istockphoto.com", "images.unsplash.com", "t4.ftcdn.net", "media.licdn.com", "encrypted-tbn0.gstatic.com", "images.pexels.com", "assets.aceternity.com",],
+    domains: ["thumbs.dreamstime.com", "media.istockphoto.com", "images.unsplash.com", "t4.ftcdn.net", "media.licdn.com", "encrypted-tbn0.gstatic.com", "images.pexels.com", "assets.aceternity.com", "images.remotePatterns",],
   },
 };
 
